75b0f27bf72fa986b2d9b9215e7c731c2751a06f
yuki ueki <karoo4413@gmail.com>
2020-02-13 12:51:00 +0900

    フォロー機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,21 @@ class User < ApplicationRecord
   has_many :dogruns
   has_many :posts
   has_many :mydogs
+  has_many :active_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :passive_relationships, foreign_key: 'followed_id', class_name: 'Relationship', dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
   end
+  #フォローしているかどうかを確認する
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+end
