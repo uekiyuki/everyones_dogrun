55b6c7b74b4cdb735ab0b627a92d4ef1cb7d8a9b
yuki ueki <karoo4413@gmail.com>
2020-02-21 18:05:48 +0900

    heroku deproy

--- app/models/mydog.rb
+++ app/models/mydog.rb
@@ -2,10 +2,13 @@ class Mydog < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :name,  presence: true, length: { maximum: 30 }
   validates :breed,  presence: true, length: { maximum: 255 }
-  validates :age,  presence: true, length: { maximum: 10 }
+  validates :age,  presence: true, length: { maximum: 2 },
+  numericality: {
+    greater_than_or_equal_to: 0,
+    less_than: 30
+  }
   validates :gender,  presence: true, length: { maximum: 10 }
   validates :image, presence: true, allow_nil: true
-
   belongs_to :user, optional: true
   enum gender: { male: 1, female: 2 }
 
